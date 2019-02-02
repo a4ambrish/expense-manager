7be93a4db0e750dc31b4af14d26155565a60918d
a4ambrish <vahan.nic@gmail.com>
2019-02-03 00:51:51 +0530

    Now Only selected item will be stored into data base

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -12,28 +12,28 @@ import { OrderDB } from '../../providers/order/OrderDB';
   templateUrl: 'order.html'
 })
 export class OrderPage {
- 
+
   currentItems: Item[];
   allOrders: OrderIndent[];
   orderIndent: OrderIndent = new OrderIndent();
-  
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController,  public itemsDB: ItemsDB, public orderDB: OrderDB) {
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public itemsDB: ItemsDB, public orderDB: OrderDB) {
     this.itemsDB.load().then(() => {
       //this.settingsReady = true;
       this.currentItems = this.itemsDB.allItems;
-      
- this.orderIndent.orderItems =  Object.assign([], this.currentItems); 
- this.orderIndent.orderDate = new Date();
-     //this._buildForm();
+
+      this.orderIndent.orderItems = Object.assign([], this.currentItems);
+      this.orderIndent.orderDate = new Date();
+      //this._buildForm();
     });
 
     this.orderDB.load().then(() => {
-          this.allOrders = this.orderDB.allItems;
+      this.allOrders = this.orderDB.allItems;
 
     });
 
-    
-}
+
+  }
 
   onChangeQty(qty) {
 
@@ -41,10 +41,10 @@ export class OrderPage {
 
     for (let num = 0; num < this.orderIndent.orderItems.length; num++) {
       if (typeof this.orderIndent.orderItems[num].qty != 'undefined' && this.orderIndent.orderItems[num].qty && this.orderIndent.orderItems[num].qty > 0) {
-        
-          this.orderIndent.orderItems[num].amount=this.orderIndent.orderItems[num].qty *
+
+        this.orderIndent.orderItems[num].amount = this.orderIndent.orderItems[num].qty *
           this.orderIndent.orderItems[num].rate;
-          tempOrderAmount = tempOrderAmount + this.orderIndent.orderItems[num].amount;
+        tempOrderAmount = tempOrderAmount + this.orderIndent.orderItems[num].amount;
       }
     }
 
@@ -52,28 +52,32 @@ export class OrderPage {
   }
 
   saveOrder() {
-  console.log("before push")
-    console.log(this.allOrders);
-  console.log("******");
-  console.log(this.orderIndent);
+
+
+    //clean up - remove items which were not ordered
+    for (let idx = 0; idx < this.orderIndent.orderItems.length; idx++) {
+      if (typeof this.orderIndent.orderItems[idx].qty != 'undefined' && this.orderIndent.orderItems[idx].qty && this.orderIndent.orderItems[idx].qty > 0) {
+        // do nothing as item has been ordered.
+      } else {
+        this.orderIndent.orderItems.splice(idx, 1);
+      }
+    }
+
+    if (typeof this.allOrders != 'undefined' && this.allOrders) {
+      this.allOrders.push(this.orderIndent);
+    } else {
+      this.allOrders = [this.orderIndent];
+      // this.allOrders[0] = this.orderIndent;
+    }
 
 
 
-if (typeof this.allOrders != 'undefined' && this.allOrders ) {
-  this.allOrders.push(this.orderIndent);
-}else{
-   this.allOrders = [this.orderIndent];
-  // this.allOrders[0] = this.orderIndent;
-}
-
-  console.log("after push")
-    console.log(this.allOrders);
     this.orderDB.setAll(this.allOrders);
     //this.orderDB.save();
-  
+
     let currentIndex = this.navCtrl.getActive().index;
     this.navCtrl.push("ContentPage").then(() => {
-        this.navCtrl.remove(currentIndex);
+      this.navCtrl.remove(currentIndex);
     });
   }
 }
